52a9e00b434cc7413db9193e13c6efee541ff62f
Tobias Reich <tobias.reich.ich@gmail.com>
2015-04-19 16:22:28 +0200

    Converted to vanilla js, added persistent and custom text mode

--- src/scripts/basicPlaceholder.js
+++ src/scripts/basicPlaceholder.js
@@ -3,78 +3,103 @@ window.basicPlaceholder = {
 	_errorClass: 'error',
 	_iconHTML: '<span class="fa fa-warning"></span>',
 
-	_dom(elem) {
-
-		if (elem===undefined)	return $('.basicPlaceholder')
-		else					return $('.basicPlaceholder').find(elem)
-
-	},
-
 	init() {
 
-		basicPlaceholder._dom('input').each(function() { basicPlaceholder._add($(this)) })
-		basicPlaceholder._dom('input').on('keyup click', basicPlaceholder._onKeyUp)
+		var inputs = document.querySelectorAll('.basicPlaceholder input')
+
+		for (let i = 0; i < inputs.length; ++i) {
+
+			let input = inputs[i]
+
+			// Add placeholder to all existing inputs
+			basicPlaceholder._add(input)
+
+			// Update placeholder
+			input.onclick = basicPlaceholder._onChange
+			input.onkeyup = basicPlaceholder._onChange
+
+		}
 
 	},
 
-	_isError($input) {
+	_isError(input) {
 
-		return $input.hasClass(basicPlaceholder._errorClass)
+		return input.classList.contains(basicPlaceholder._errorClass)
 
 	},
 
-	_onKeyUp() {
+	_isPersistent(input) {
 
-		var $input = $(this)
+		var persistent = input.getAttribute('data-basicPlaceholder-persistent')
+
+		if (persistent!==null&&persistent!=='false')	return true
+		else											return false
+
+	},
+
+	_onChange() {
+
+		var input	= this,
+			isError	= basicPlaceholder._isError(input)
 
 		// Remove error placeholder
-		if (basicPlaceholder._isError($input)===true) basicPlaceholder._remove($input)
+		if (isError===true) basicPlaceholder._remove(input)
 
 		// Show basicPlaceholder when input contains chars
-		if ($input.val().length>0)	basicPlaceholder._add($input)
-		else						basicPlaceholder._remove($input)
+		if (input.value.length>0)	basicPlaceholder._add(input)
+		else						basicPlaceholder._remove(input)
 
 	},
 
-	_add($input) {
+	_add(input) {
 
-		var text	= null,
-			html	= null,
-			isError	= basicPlaceholder._isError($input)
+		var text			= null,
+			customText		= input.getAttribute('data-basicPlaceholder-text'),
+			wrapper			= input.parentElement,
+			isError			= basicPlaceholder._isError(input),
+			isPersistent	= basicPlaceholder._isPersistent(input),
+			html			= null
 
 		// Do not add a placeholder when ...
-		// 1) input empty and not an error
+		// 1) input empty, not an error and not persistent
 		// 2) a placeholder has already been added to the input
-		if ($input.val().length===0&&isError===false)		return false
-		if ($input.attr('data-basicPlaceholder')==='true')	return false
+		if (isPersistent===false&&input.value.length===0&&isError===false)	return false
+		if (input.getAttribute('data-basicPlaceholder')==='true')			return false
 
 		// Set placeholder text
 		if (isError===true)	text = basicPlaceholder._iconHTML
-		else				text = $input.attr('placeholder') || null
+		else				text = input.getAttribute('placeholder') || null
+
+		// Set custom text when available
+		if (customText!==null&&customText.length>0) text = customText
 
 		// Prevent undefined title
 		if (text===null) return false
 
 		// Render and add HTML
 		html = basicPlaceholder._render(text)
-		$input.after(html)
-		$input.attr('data-basicPlaceholder', true)
+		wrapper.insertAdjacentHTML('beforeend', html)
+
+		// Set attribute
+		input.setAttribute('data-basicPlaceholder', true)
 
 		return true
 
 	},
 
-	_remove($input) {
+	_remove(input) {
 
-		var $wrapper		= $input.parent(),
-			$placeholder	= $wrapper.find('.basicPlaceholder__placeholder'),
-			persistent		= $input.attr('data-basicPlaceholder-persistent')
+		var wrapper			= input.parentElement,
+			placeholder		= wrapper.querySelector('.basicPlaceholder__placeholder'),
+			isPersistent	= basicPlaceholder._isPersistent(input)
 
-		if (persistent!==undefined) return false
+		if (isPersistent===true)	return false
+		if (placeholder===null)		return false
 
-		$placeholder.remove()
-		$input.removeClass(basicPlaceholder._errorClass)
-		$input.attr('data-basicPlaceholder', false)
+		placeholder.remove()
+
+		input.classList.remove(basicPlaceholder._errorClass)
+		input.setAttribute('data-basicPlaceholder', false)
 
 		return true
 
